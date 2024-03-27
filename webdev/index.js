360be7f47ddc157aab4565a79574cab0eea7f439
owndji <155955094+owndji@users.noreply.github.com>
2024-03-27 23:39:27 +0100

    Add files via upload

---
+++ webdev/index.js
@@ -0,0 +1,9 @@
+// const p1 = document.querySelector('.p1');
+
+// p1.addEventListener('mouseenter', () => {
+//   p1.style.transform = 'scale(1.2)';
+// });
+
+// p1.addEventListener('mouseleave', () => {
+//   p1.style.transform = 'scale(1)';
+// });
